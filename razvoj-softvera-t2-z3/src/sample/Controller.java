a1612bfc34e090ccf54398ef8724c10fd42eaa5f
msmrkovic1 <msmrkovic1@etf.unsa.ba>
2020-03-18 12:57:55 +0100

    dodaj izgled sample.fx-a i funkcijonalnost objasnjenu u tutorijalu

--- razvoj-softvera-t2-z3/src/sample/Controller.java
+++ razvoj-softvera-t2-z3/src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField unos;
+
+    public void Submit(ActionEvent actionEvent) {
+        String str = unos.getText();
+        String[] rijeci = str.split(" ", -2);
+
+        int max = 0, maxIndex = 0;
+        for (int i = 0; i < rijeci.length; i++) {
+
+            if (max < rijeci[i].length()) { max = rijeci[i].length(); maxIndex = i; }
+        }
+        str = "";
+        for (int i = 0; i < rijeci.length; i++) {
+            if(i == maxIndex) str += rijeci[i].toUpperCase() + " ";
+            else str += rijeci[i] + " ";
+        }
+
+        unos.setText(str);
+    }
 }
